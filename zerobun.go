3c864223fa7c3a73ad52750984fc70cb73115da5
Collin Smallegan <cmsmallegan@comsma.com>
2023-11-15 12:17:24 -0500

    hello code

---
+++ zerobun.go
@@ -0,0 +1,49 @@
+package zerobun
+
+import (
+	"context"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/uptrace/bun"
+)
+
+const (
+	OperationFieldName     = "operation"
+	OperationTimeFieldName = "operation_time_ms"
+)
+
+type QueryHook struct {
+	bun.QueryHook
+
+	logger       *zerolog.Logger
+	slowDuration time.Duration
+}
+
+type QueryHookOptions struct {
+	Logger       *zerolog.Logger
+	SlowDuration time.Duration
+}
+
+func NewQueryHook(options QueryHookOptions) QueryHook {
+	return QueryHook{
+		logger:       options.Logger,
+		slowDuration: options.SlowDuration,
+	}
+}
+
+func (qh QueryHook) BeforeQuery(ctx context.Context, event *bun.QueryEvent) context.Context {
+	return ctx
+}
+
+func (qh QueryHook) AfterQuery(ctx context.Context, event *bun.QueryEvent) {
+	queryDuration := time.Since(event.StartTime)
+
+	fields := map[string]interface{}{
+		OperationFieldName:     event.Operation(),
+		OperationTimeFieldName: queryDuration.Milliseconds(),
+	}
+
+	qh.logger.Debug().Fields(fields)
+
+}
